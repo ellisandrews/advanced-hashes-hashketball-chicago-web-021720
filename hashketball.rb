775a62ecf117052f76746e7e09d71bc865519fac
Ellis Andrews <ellisandrews1@gmail.com>
2020-02-06 16:34:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -111,7 +111,7 @@ def game_hash
           slam_dunks: 0
         },
         {
-          player_name: "",
+          player_name: "Kemba Walker",
           number: 0,
           shoe: 0,
           points: 0,
