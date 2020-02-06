0bdcbc0c7ab36499081d71903d64e9a4b902bf08
Ellis Andrews <ellisandrews1@gmail.com>
2020-02-06 16:30:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -38,7 +38,7 @@ def game_hash
           slam_dunks: 15
         },
         {
-          player_name: "",
+          player_name: "Mason Plumlee",
           number: 0,
           shoe: 0,
           points: 0,
