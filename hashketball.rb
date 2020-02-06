1464351cb39f5d220d73eb446b48a22cb2dc81b6
Ellis Andrews <ellisandrews1@gmail.com>
2020-02-06 16:33:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -95,9 +95,9 @@ def game_hash
           points: 24,
           rebounds: 12,
           assists: 12,
-          steals: ,
-          blocks: 0,
-          slam_dunks: 0
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
         },
         {
           player_name: "",
