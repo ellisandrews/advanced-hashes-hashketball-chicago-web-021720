c4ff852d8f8015a1caf1e8906b803880804c5647
Ellis Andrews <ellisandrews1@gmail.com>
2020-02-06 17:38:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -215,5 +215,16 @@ end
 
 
 def most_points_scored
+  points = 0 
+  highest_scoring_player = ""
+  
+  game_hash.each_value do |team_data|
+    team_data[:players].each do |player|
+      if player[:points] > points
+        
+      end
+    end
+  end
+  
   
 end
