b4c58be863604990092f161b0456ba39d7aeec6d
Ellis Andrews <ellisandrews1@gmail.com>
2020-02-06 16:53:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -149,17 +149,6 @@ def shoe_size(player_name)
   lookup_player_stat(player_name, :shoe)
 end
 
-# def num_points_scored(player_name)
-#   data = game_hash()
-  
-#   data.each do |team, team_data|
-#     team_data[:players].each do |player|
-#       if player[:player_name] == player_name
-#         return player[:points]
-#       end
-#     end
-#   end
-# end
 
 
 
