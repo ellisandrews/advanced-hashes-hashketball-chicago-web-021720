60181580aed2ee0cac94fb8bbdff568040576e07
Ellis Andrews <ellisandrews1@gmail.com>
2020-02-06 16:29:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,12 +19,12 @@ def game_hash
           player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
-          points: 0,
-          rebounds: 0,
-          assists: 0,
-          steals: 0,
-          blocks: 0,
-          slam_dunks: 0
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
         },
         {
           player_name: "",
